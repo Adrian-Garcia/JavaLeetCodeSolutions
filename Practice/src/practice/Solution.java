369f8fb8e309ffd96f342701f92eaeb590dc8deb
Adrián García <31387681+Adrian-Garcia@users.noreply.github.com>
2019-05-23 09:50:54 -0500

    Added files via upload

---
+++ Practice/src/practice/Solution.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+/**
+ *
+ * @author adria
+ */
+public class Solution {
+    
+    public Solution() {}
+    
+    public boolean isPalindrome(int x) {
+        return true;
+    }
+}
